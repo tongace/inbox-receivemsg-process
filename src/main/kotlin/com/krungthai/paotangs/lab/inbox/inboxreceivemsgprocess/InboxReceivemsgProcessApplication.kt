f16fdbc318c928bfa3537cbd5006b273b498d4d5
chairat <chairat@HQ8636630219N1.local>
2020-10-08 13:47:21 +0700

    innitial version

---
+++ src/main/kotlin/com/krungthai/paotangs/lab/inbox/inboxreceivemsgprocess/InboxReceivemsgProcessApplication.kt
@@ -0,0 +1,11 @@
+package com.krungthai.paotangs.lab.inbox.inboxreceivemsgprocess
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InboxReceivemsgProcessApplication
+
+fun main(args: Array<String>) {
+	runApplication<InboxReceivemsgProcessApplication>(*args)
+}
